0101e255913a11ffee77d58081fa173c6efb0804
K.H Lee <beta1360@naver.com>
2019-06-13 19:58:56 +0900

    (+) add #09: basic UI - Login UI & ajax

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,12 +1,44 @@
 $(document).ready(function(){
     $(function(){
         $('#loginOK').click(function(){
-                        // check log in logic
+            // check log in logic
+            var id = $('#loginID').val();
+            var pwd = $('#loginPwd').val();
 
-            // if success
-            alert("Success to log in!");
-            initLogInForm();
-            // else
+            if(id.length == 0 || pwd.length == 0)
+                alert("Please input user id or password in this boxes.");
+
+            else {
+                var status_code;
+                var message;
+
+                $.ajax({
+                    type:'POST'
+                    , cache: false
+                    , url:"http://localhost:8000/api/login"
+                    , data: JSON.stringify({
+                        user_id: id,
+                        user_pw: pwd
+                    })
+                    , async: false
+                    , contentType : 'application/json; charset=utf-8'
+                    , dataType: 'json'
+                    , success: function(data){
+                        status_code = data.code;
+                        message = data.message;
+                    }, error: function(data){
+                        status_code = -1;
+                    }
+                });
+
+                if(status_code == 200) {
+                    alert(message);
+                    initLogInForm();
+                } else if(status_code == 601)
+                    alert(message);
+                else
+                    alert("Error...");
+            }
         });
     });
 })
