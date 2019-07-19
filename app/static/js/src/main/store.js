ddf6fb812efbba6eb51b57a777677d94c6ee8ec1
K.H Lee <beta1360@naver.com>
2019-07-19 18:04:31 +0900

    Delete store.js

--- app/static/js/src/main/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
-import reducers from './store/index';
-
-const store = createStore(reducers, applyMiddleware(thunk));
-
-export default store;
